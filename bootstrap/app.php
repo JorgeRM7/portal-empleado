e249ce79c1107d93b23ad4d58b5131683c5041ce
abdielGraciano <luisabdiel.gv@gmail.com>
2026-04-24 09:24:27 -0600

    act

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,6 +12,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->alias([
+            'terms' => \App\Http\Middleware\EnsureTermsAccepte::class,
+        ]);
         $middleware->web(append: [
             \App\Http\Middleware\HandleInertiaRequests::class,
             \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
