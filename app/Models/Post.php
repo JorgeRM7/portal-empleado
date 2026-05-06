8b2c88abc799e6878b2f295c92c6bd87af6a242c
alfonsoGallardoGO <jose.gallardo@grupo-ortiz.com>
2026-05-06 14:07:14 -0600

    fix: mostrar noticias sin eliminar

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -3,10 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Support\Facades\Auth;
 
 class Post extends Model
 {
+    use SoftDeletes;
     protected $table = 'news';
 
     public function user()
