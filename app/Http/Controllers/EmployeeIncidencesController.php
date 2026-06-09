11e6fe7a812ddb1a3e70ac3d6a9d72b5b791cacd
alfonsoGallardoGO <jose.gallardo@grupo-ortiz.com>
2026-06-09 08:57:55 -0600

    feat: horas txt con punto decimal

--- app/Http/Controllers/EmployeeIncidencesController.php
+++ app/Http/Controllers/EmployeeIncidencesController.php
@@ -178,7 +178,7 @@ class EmployeeIncidencesController
             ]);
 
             $week = $getWeekData($validated['singleDate']);
-            $hours = (int) $validated['txt_hours_to_register'];
+            $hours = $validated['txt_hours_to_register'];
 
             $base = [
                 "employee_id"       => $validated['employee_id'],
